fb0f68b2cdb2481bf867030873af3ca4fa916bde
kriskafebri <kriskafebri@gmail.com>
2017-08-24 09:52:43 +0700

    RecyclerView List Card

---
+++ app/src/main/java/com/jasahub/recyclerview3/model/Hotel.java
@@ -0,0 +1,20 @@
+package com.jasahub.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by SE on 23/08/2017.
+ */
+
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+
+    }
+}

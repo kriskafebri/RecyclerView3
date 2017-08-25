8c7e178033ba7048d2fa4582bec93531988becca
kriskafebri <kriskafebri@gmail.com>
2017-08-25 10:28:53 +0700

    RecyclerView Edit

--- app/src/main/java/com/jasahub/recyclerview3/MainActivity.java
+++ app/src/main/java/com/jasahub/recyclerview3/MainActivity.java
@@ -22,7 +22,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
     public static final String HOTEL = "hotel";
     public static final int REQUEST_CODE_ADD = 88;
-
+    public static final int REQUEST_CODE_EDIT = 99;
+    int itemPos;
 
     /* script diatas untuk Implementsi interface IHotelAdapter pada class MainActivity Agar dapat membuka halaman detail*/
 
@@ -104,6 +105,11 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
             Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
             mList.add(hotel);
             mAdapter.notifyDataSetChanged();
+        } else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK) {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+            mList.add(itemPos, hotel);
+            mAdapter.notifyDataSetChanged();
         }
     }
 
@@ -134,6 +140,29 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(HOTEL, mList.get(pos));
         startActivity(intent);
+
+    }
+
+    @Override
+    public void doEdit(int pos) {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
+    }
+
+    @Override
+    public void doDelete(int pos) {
+
+    }
+
+    @Override
+    public void doFav(int pos) {
+
+    }
+
+    @Override
+    public void doShare(int pos) {
 
     }
 }
